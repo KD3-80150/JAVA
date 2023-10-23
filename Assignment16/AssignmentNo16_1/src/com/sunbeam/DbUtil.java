dc93f21edf4a0968b0a2c9a43a46211314926fed
ruchadharak <rvsdharak@gmail.com>
2023-10-23 18:35:17 +0530

    Assignment16 Uploaded

---
+++ Assignment16/AssignmentNo16_1/src/com/sunbeam/DbUtil.java
@@ -0,0 +1,25 @@
+package com.sunbeam;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+public class DbUtil 
+{
+	public static final String DB_DRIVER = "com.mysql.cj.jdbc.Driver";
+	public static final String DB_URL = "jdbc:mysql://localhost:3306/rutu1";
+	public static final String DB_USER = "KD3_80136rutujahatgine";
+	public static final String DB_PASSWD = "manager";
+	
+	static {
+		try {
+			Class.forName(DB_DRIVER);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+	}
+
+	public static Connection getConnection() throws SQLException {
+		Connection con = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWD);
+		return con;
+	}
+}
---
+++ Assignment16/AssignmentNo16_2/src/com/sunbeam/DbUtil.java
@@ -0,0 +1,26 @@
+package com.sunbeam;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DbUtil 
+{
+	private static String DB_DRIVER="com.mysql.cj.jdbc.Driver";
+	private static String DB_URL="jdbc:mysql://localhost:3306/rutu1";
+	private static String DB_USER="KD3_80136rutujahatgine";
+	private static String DB_PASSWORD="manager";
+	
+	static {
+		try {
+			Class.forName(DB_DRIVER);
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+	}
+	
+	public static Connection getConnection() throws SQLException {
+		Connection con = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+		return con;
+	}
+}
